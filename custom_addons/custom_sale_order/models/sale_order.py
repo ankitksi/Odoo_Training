6147fbf465086b37899758236ef340e786d1b1b0
ankitksi <ankit.suryawanshi@ksolves.com>
2024-12-04 14:15:29 +0530

    This commit created a server action "Merge Sale Order" -Merges the sale orders of the same customer. -Checks for validation i.e. if the selected orders do not belong to same customer then it raises validation error.

--- custom_addons/custom_sale_order/models/sale_order.py
+++ custom_addons/custom_sale_order/models/sale_order.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -21,4 +22,36 @@ class SaleOrder(models.Model):
                 'default_sale_order_id': self.id,
                 'default_sale_order_line_ids': [(6, 0, self.order_line.ids)],
             }
+        }
+
+    @api.model
+    def merge_sale_orders_action(self):
+        selected_orders = self.browse(self.env.context.get('active_ids', []))
+
+        # Check if orders belong to different customers
+        customers = selected_orders.mapped('partner_id')
+        if len(customers) > 1:
+            raise ValidationError(
+                "You have selected orders from different customers. Please select orders from the same customer.")
+
+        # Create a new sale order for the customer
+        partner = selected_orders[0].partner_id
+        new_order = self.create({
+            'partner_id': partner.id,
+            'order_line': [(0, 0, {
+                'product_id': line.product_id.id,
+                'product_uom_qty': line.product_uom_qty,
+                'price_unit': line.price_unit,
+                'tax_id': [(6, 0, line.tax_id.ids)],
+            }) for order in selected_orders for line in order.order_line]
+        })
+
+
+        # Return an action to open the newly created sale order in the form view
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'sale.order',
+            'view_mode': 'form',
+            'res_id': new_order.id,
+            'target': 'current',
         }
